fd4adef8cf5139932c3179e4d5f44b8e01552f6f
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2023-05-14 02:37:37 +0700

    adding berada page

--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -2,16 +2,22 @@
 
 @section('content')
     <section class="beranda">
-        <div class="jumbo h-100">
-            <div class="row py-5">
-                <div class="col-sm-12">
-                    <h1>CV HIDRO SINERGI UTAMA</h1>
+        <div class="jumbo container">
+            <div class="row">
+                <div class="col-sm-6 align-self-center">
+                    <div class="row jumbo-text">
+                        <div class="col-sm-12">
+                            <h1>CV HIDRO</h1>
+                            <h1>SINERGI UTAMA</h1>
+                            <p>Sinergi Dari Desa Untuk Indonesia</p>
+                        </div>
+                        <div class="col-sm-12 mt-5">
+                            <a href="">Get Started</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-12">
-                    <p>Sinergi Dari Desa Untuk Indonesia</p>
-                </div>
-                <div class="col-sm-12 d-flex justify-content-center">
-                    <a href="">Get Started</a>
+                <div class="col-sm-6">
+                    <img src="{{ asset('assets/images/beranda.png') }}" alt="beranda" class="w-auto">
                 </div>
             </div>
         </div>
